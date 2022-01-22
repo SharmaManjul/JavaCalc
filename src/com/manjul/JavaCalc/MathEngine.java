cbfae69dc4b5b8024fc1ac0172b670ec04f18825
SharmaManjul <manjul199912@gmail.com>
2022-01-22 16:34:37 -0600

    Included documentation.

--- src/com/manjul/JavaCalc/MathEngine.java
+++ src/com/manjul/JavaCalc/MathEngine.java
@@ -6,6 +6,14 @@ public class MathEngine {
     private char opCode;
     private double result;
 
+    /*
+     ENCAPSULATION:
+     Process of wrapping code & data into a single unit in a way that hides and controls the data while making the class
+     easy to test.
+     Getters and setters here allow us to control and hide the left, right and opCode variables. Only using a getter for
+     the result allows us to protect the final data and restrict any other class from accessing it.
+    */
+
     public double getLeftVal(){return leftVal;}
     public void setLeftVal(double leftVal){this.leftVal = leftVal;}
     public double getRightVal(){return rightVal;}
