6578e0c05dfd36e8a5d9acf15722b065b9c62b98
SharmaManjul <manjul199912@gmail.com>
2022-01-21 19:28:18 -0600

    Initial hello world commit.

---
+++ src/com/manjul/JavaCalc/Main.java
@@ -0,0 +1,9 @@
+package com.manjul.JavaCalc;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world! This is the best practice java calculator.");
+    }
+}
